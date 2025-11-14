a37d2c441a6cabbd5942e2cf38c4d65f10fea21c
Jo <johannesrieck@kabelmail.de>
2025-11-14 23:45:21 +0100

    Version 2.0.9

--- prototypes/entities/b29.lua
+++ prototypes/entities/b29.lua
@@ -117,6 +117,7 @@ data:extend({
         utils.layerMaker(modname, "", "b29",               2290, {1,3}, {2,2}, 32, 1, offset({0,0})),
         utils.layerMaker(modname, "", "b29_rotor",  {1290,1000}, {3,1}, {3,4}, 32, 1, offset({-50, -105})),
         utils.layerMaker(modname, "shadow", "b29",         2290, {1,3}, {2,2}, 32, 2, offset({0,0})),
+        utils.layerMaker(modname, "shadow", "b29_rotor",{1450,910},{3,1},{3,4},32, 2, offset({0,0})),
         --utils.layerMaker(modname, "shadow", "b29_rotor",  2290, {3,1}, {3,4}, 32, 2),
       },
     },
